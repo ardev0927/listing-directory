039657376fefb67d8d2c6c2897d541e7ec02deff
Flutter dev <52046319+ardev0927@users.noreply.github.com>
2023-08-01 09:55:45 +0300

    Revert "Add flutter bloc to app main"
    This reverts commit a0318a7e407bef17b4bc8033fda54c69a73ca1b5.

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'utils/logger.dart';
-
-class AppBlocObserver extends BlocObserver {
-  @override
-  void onChange(BlocBase bloc, Change change) {
-    super.onChange(bloc, change);
-    UtilLogger.log('BLOC ONCHANGE', change);
-  }
-
-  @override
-  void onEvent(Bloc bloc, Object? event) {
-    UtilLogger.log('BLOC EVENT', event);
-    super.onEvent(bloc, event);
-  }
-
-  @override
-  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    UtilLogger.log('BLOC ERROR', error);
-    super.onError(bloc, error, stackTrace);
-  }
-
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    UtilLogger.log('BLOC TRANSITION', transition);
-    super.onTransition(bloc, transition);
-  }
-}
 
 void main() {
-  Bloc.observer = AppBlocObserver();
   runApp(const MyApp());
 }
 
