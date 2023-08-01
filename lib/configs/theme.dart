13e9a56adf9c8da737708267c21137c96dace945
Flutter dev <52046319+ardev0927@users.noreply.github.com>
2023-08-01 16:03:03 +0300

    Revert "Revert "Add flutter bloc to app main""
    This reverts commit 039657376fefb67d8d2c6c2897d541e7ec02deff.

---
+++ lib/configs/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+enum DarkOption { dynamic, alwaysOn, alwaysOff }
+
+class AppTheme {
+  /// Default font
+  static const String defaultFont = "ProximaNova";
+
+  /// List Font support
+  static final List<String> fontSupport = [
+    "OpenSans",
+    "ProximaNova",
+    "Raleway",
+    "Roboto",
+    "Merriweather",
+  ];
+}
