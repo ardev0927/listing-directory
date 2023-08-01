a0318a7e407bef17b4bc8033fda54c69a73ca1b5
Flutter dev <52046319+ardev0927@users.noreply.github.com>
2023-08-01 09:55:21 +0300

    Add flutter bloc to app main

---
+++ lib/blocs/application/application_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:listing_directory/configs/preferences.dart';
+import 'package:meta/meta.dart';
+
+import 'application_state.dart';
+
+class ApplicationCubit extends Cubit<ApplicationState> {
+  ApplicationCubit() : super(ApplicationState.loading);
+
+  /// On Setup Application
+  Future<void> onSetup() async {
+    /// Notify loading
+    emit(ApplicationState.loading);
+
+    /// Setup SharedPreferences
+    await Preferences.setPreferences();
+
+    /// Get old Theme & Font & Language
+    final oldTheme = Preferences.getString(Preferences.theme);
+    final oldFont = Preferences.getString(Preferences.font);
+    final oldLanguage = Preferences.getString(Preferences.language);
+    final oldDarkOption = Preferences.getString(Preferences.darkOption);
+    final oldBusiness = Preferences.getString(Preferences.business);
+  }
+}
